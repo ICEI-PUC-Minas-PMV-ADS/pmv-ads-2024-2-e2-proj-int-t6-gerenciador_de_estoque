30f4f10a0e46d22f613b1331792d32d2b3f6a82f
adriansodr3 <adriansodree@gmail.com>
2024-10-26 11:13:40 -0300

    Incluindo View e controller Fornecedores

---
+++ src/StockUp/Controllers/FornecedoresController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StockUp.Models;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StockUp.Controllers
+{
+    [Authorize]
+    public class FornecedoresController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public FornecedoresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
